32106dc15d3de74b5fada3ea9c1c1433e374b73a
Lorenzo Isidori <lorenzo.isidori21@gmail.com>
2025-03-29 11:57:20 +0100

    web api ticketByCarrier and searchTickets

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -43,82 +43,111 @@ namespace TicketsB2C.Controllers
 
             return Ok(tickets);
         }
-
-        // GET: api/Tickets/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Ticket>> GetTicket(int id)
+        
+        /// <summary>
+        /// GET: api/Tickets/TicketsByCarrier
+        /// </summary>
+        /// <param name="carrier">Carrier name</param>
+        /// <returns>All available tickets sold from <param name="carrier"></param></returns> 
+        /// <response code="400">Please, insert a carrier</response>
+        /// <response code="404">"No available tickets for that carrier"</response>
+        [HttpGet("TicketsByCarrier")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        public async Task<ActionResult<IEnumerable<TicketDto>>> GetTicketsByCarrier(string carrier = "")
         {
-            var ticket = await _context.Tickets.FindAsync(id);
-
-            if (ticket == null)
-            {
-                return NotFound();
-            }
-
-            return ticket;
-        }
-
-        // PUT: api/Tickets/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTicket(int id, Ticket ticket)
-        {
-            if (id != ticket.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(ticket).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TicketExists(id))
+            if (string.IsNullOrEmpty(carrier))
+                return BadRequest(new ProblemDetails
                 {
-                    return NotFound();
-                }
-                else
+                    Status = StatusCodes.Status400BadRequest,
+                    Title = "Invalid request",
+                    Detail = "Please, insert a carrier."
+                });
+            var tickets = await _context.Tickets
+                .Include(t => t.Carrier)
+                .Include(t => t.Destination)
+                .Include(t => t.Departure)
+                .Include(t => t.Type)
+                .Where(t => t.Carrier.Name == carrier)
+                .Select(t => new TicketDto()
                 {
-                    throw;
-                }
-            }
+                    Id = t.Id,
+                    Price = t.Price,
+                    Departure = t.Departure.Name,
+                    Destination = t.Destination.Name,
+                    TransportType = t.Type.Description,
+                    Carrier = t.Carrier.Name
+                })
+                .ToListAsync();
 
-            return NoContent();
-        }
-
-        // POST: api/Tickets
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Ticket>> PostTicket(Ticket ticket)
-        {
-            _context.Tickets.Add(ticket);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTicket", new { id = ticket.Id }, ticket);
-        }
-
-        // DELETE: api/Tickets/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTicket(int id)
-        {
-            var ticket = await _context.Tickets.FindAsync(id);
-            if (ticket == null)
+            if (!tickets.Any())
             {
-                return NotFound();
+                return NotFound(new ProblemDetails
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Title = "Item not found",
+                    Detail = "No available tickets for that carrier"
+                });
             }
-
-            _context.Tickets.Remove(ticket);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return Ok(tickets);
         }
-
-        private bool TicketExists(int id)
+        /// <summary>
+        /// GET: api/Tickets/SearchTickets
+        /// </summary>
+        /// <param name="departure">Departure city</param>
+        /// <param name="destination">Destination city</param>
+        /// <returns>All available ticket for <param name="departure"></param> and <param name="destination"></param> provided</returns>
+        /// <response code="400">Please, insert a departure/destination</response>
+        /// <response code="404">"No available tickets for those cities"</response>
+        [HttpGet("SearchTickets")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        public async Task<ActionResult<IEnumerable<TicketDto>>> SearchTickets(string departure = "", string destination = "")
         {
-            return _context.Tickets.Any(e => e.Id == id);
+            if (string.IsNullOrEmpty(departure))
+                return BadRequest(new ProblemDetails
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Title = "Invalid request",
+                    Detail = "Please, insert a departure"
+                });
+            if (string.IsNullOrEmpty(destination))
+                return BadRequest(new ProblemDetails
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Title = "Invalid request",
+                    Detail = "Please, insert a destination"
+                });
+
+            var tickets = await _context.Tickets
+                .Include(t => t.Carrier) // Eager loading to include Carrier data
+                .Include(t => t.Destination) // Eager loading to include Carrier data
+                .Include(t => t.Departure) // Eager loading to include Carrier data
+                .Include(t => t.Type) // Eager loading to include Carrier data
+                .Where(t => t.Departure.Name == departure && t.Destination.Name == destination)
+                .Select(t => new TicketDto()
+                {
+                    Id = t.Id,
+                    Price = t.Price,
+                    Departure = t.Departure.Name,
+                    Destination = t.Destination.Name,
+                    TransportType = t.Type.Description,
+                    Carrier = t.Carrier.Name
+                })
+                .ToListAsync();
+
+            if (!tickets.Any())
+            {
+                return NotFound(new ProblemDetails
+                {
+                    Status = StatusCodes.Status404NotFound,
+                    Title = "Item not found",
+                    Detail = "No available tickets for those cities"
+                });
+            }
+            return Ok(tickets);
         }
     }
 }
